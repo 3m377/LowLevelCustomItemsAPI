73a5328c247b12e2d907a76dba35319a1362c8f8
3m377 <emettmiles@outlook.com>
2025-09-01 22:25:08 -0700

    fuck

--- LowLevelCustomItemsAPI/Plugin.cs
+++ LowLevelCustomItemsAPI/Plugin.cs
@@ -8,7 +8,7 @@ public class Plugin : LabApi.Loader.Features.Plugins.Plugin
 	public override string Name => "LowLevelCustomItemsAPI";
 	public override string Description => "A low-level API for custom items.";
 	public override string Author => "3m377";
-	public override Version Version => new(1, 0, 0);
+	public override Version Version => new(1, 0, 1);
 	public override Version RequiredApiVersion => LabApiProperties.CurrentVersion;
 
 	public override void Enable()
